6eba00a3ebd4be0837fa36591c4e1b8888bcef84
Harshith Sai V <68597202+godfather2327@users.noreply.github.com>
2022-08-08 00:14:41 +0530

    Time: 0 ms (100.00%), Space: 44 MB (12.27%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -10,9 +10,9 @@ class Solution {
             while(low<=high)
             {
                 int mid=(low+high)/2;
-                if(nums[mid]==target)
-                    return mid;
-                else if(nums[mid]<target)
+                // if(nums[mid]==target)
+                //     return mid;
+                if(nums[mid]<target)
                     low=mid+1;
                 else
                     high=mid-1;
