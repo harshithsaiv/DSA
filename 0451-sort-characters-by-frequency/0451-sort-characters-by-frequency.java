f232a307e3a4519f6b4a355cb422f821652a2ce5
Harshith Sai V <68597202+godfather2327@users.noreply.github.com>
2024-02-06 22:00:40 -0800

    Time: 67 ms (19.29%), Space: 45.6 MB (48.36%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,44 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class Solution {
+    public String frequencySort(String s) {
+        LinkedHashMap<Character, Integer> alphabets = new LinkedHashMap<>();
+        StringBuilder result = new StringBuilder();
+
+        // Initialize the map with all characters and frequencies set to 0
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphabets.put(ch, 0);
+        }
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            alphabets.put(ch, 0);
+        }
+        for(char ch = '0'; ch<='9';ch++)
+        {
+            alphabets.put(ch,0);
+        }
+
+        // Update the frequencies based on the input string
+        int max = 0;
+        for (char c : s.toCharArray()) {
+            if (alphabets.containsKey(c)) {
+                int count = alphabets.get(c);
+                alphabets.put(c, count + 1);
+                max = Math.max(max, count + 1);
+            }
+        }
+
+        // Construct the result string
+        for (int j = max; j >= 1; j--) {
+            for (Map.Entry<Character, Integer> entry : alphabets.entrySet()) {
+                if (entry.getValue() == j) {
+                    for (int i = 0; i < j; i++) {
+                        result.append(entry.getKey());
+                    }
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
