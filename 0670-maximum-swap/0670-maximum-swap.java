c1e6521e10ac3e1de551edd8c9f2c6dbfc522265
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2024-10-17 14:42:57 -0700

    Time: 1 ms (30.39%), Space: 40.5 MB (39.70%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -5,11 +5,13 @@ class Solution {
         for(int i=0;i<str.length();i++){
             for(int j=i+1;j<str.length();j++){
                 char [] cnum = str.toCharArray();
-                char temp = cnum[i];
-                cnum[i]=cnum[j];
-                cnum[j]=temp;
-                int x = Integer.parseInt(new String(cnum));
-                max = Math.max(x,max);
+                if(cnum[j]>cnum[i]){
+                    char temp = cnum[i];
+                    cnum[i]=cnum[j];
+                    cnum[j]=temp;
+                    int x = Integer.parseInt(new String(cnum));
+                     max = Math.max(x,max);
+                } 
             }
         }
         return max;
