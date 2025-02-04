a642c9651db5ccfcb41bf9f4a485a2d9d2924e66
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-02-04 14:38:18 -0800

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 1 ms (83.58%), Space: 41.7 MB (72.24%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,19 +1,19 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String result="";
-        for(int i=0;i<word1.length()|| i<word2.length();i++)
-        {
-            if(i<word1.length())
-            {
-                result+=word1.charAt(i);
-            }
-            if(i<word2.length())
-            {
-                result+=word2.charAt(i);
+        StringBuilder res=new StringBuilder();
+        int j=0;
+
+        for(int i=0;i<word1.length();i++){
+            res.append(word1.charAt(i));
+            if(j<word2.length()){
+                res.append(word2.charAt(j));
+                j++;
             }
         }
-
-        return result;
-        
+        while(j<word2.length()){
+                res.append(word2.charAt(j));
+                j++;
+            }
+        return res.toString();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DSA
 Keeping track of the problems I am solving to keep me motivated. !! 
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Two Pointers
+|  |
+| ------- |
+| [1894-merge-strings-alternately](https://github.com/harshithsaiv/DSA/tree/master/1894-merge-strings-alternately) |
+## String
+|  |
+| ------- |
+| [1894-merge-strings-alternately](https://github.com/harshithsaiv/DSA/tree/master/1894-merge-strings-alternately) |
+<!---LeetCode Topics End-->
