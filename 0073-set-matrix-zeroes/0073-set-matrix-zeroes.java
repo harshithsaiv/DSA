4b908b1319bc79cfa15b6235c2a468e8f9f3294d
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-02-18 14:35:04 -0800

    Time: 2 ms (19.23%), Space: 45.7 MB (65.53%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,23 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int R = matrix.length;
-        int C = matrix[0].length;
-        Set<Integer> rows = new HashSet<Integer>();
-        Set<Integer> cols = new HashSet<Integer>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        HashSet<Integer> row = new HashSet<>();
+        HashSet<Integer> col = new HashSet<>();
 
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    cols.add(j);
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row.add(i);
+                    col.add(j);
                 }
             }
         }
 
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (rows.contains(i) || cols.contains(j)) {
-                    matrix[i][j] = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row.contains(i) || col.contains(j)){
+                    matrix[i][j]=0;
                 }
             }
         }
