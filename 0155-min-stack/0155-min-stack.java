c18b9e1e8479fc7aabf719610f0c71932513752d
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-04-28 14:44:51 -0700

    0155-MIN-STACK Stats: Time: 4 ms (97.34%), Space: 44.8 MB (82.92%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,33 +1,89 @@
 class MinStack {
 
-    private Stack<int[]> stk;
+
+    private Stack<Long> stk;
+    private long min =0;
     public MinStack() {
         stk = new Stack<>();
+        min=Integer.MAX_VALUE;
     }
     
-    public void push(int val) {
+    public void push(int val){
         if(stk.isEmpty()){
-            stk.push(new int[]{val,val});
+            min =val;
+            stk.push((long)val);
         }
         else{
-            int prev_min = stk.peek()[1];
-            stk.push(new int[]{val,Math.min(val,prev_min)});
+            if(val>min){
+                stk.push((long)val);
+            }
+            else{
+                stk.push((long)2*val-min);
+                min = val;
+            }
         }
     }
     
     public void pop() {
+        if(stk.isEmpty()){
+            return;
+        }
+
+        long n = stk.peek();
         stk.pop();
+        if(n<min){
+            min = 2*min-n;
+        }
+        if (stk.isEmpty()) {   
+            min = Integer.MAX_VALUE;
+        }
     }
     
     public int top() {
-
-        return stk.peek()[0];
+        long n = stk.peek();
+        if(n<min){
+            return (int)min;
+        }
+        else{
+            return (int)n;
+        }
     }
     
     public int getMin() {
-        return stk.peek()[1];
+        return (int)min;
     }
 }
+
+// This method has Time complexity of O(1) but Space complexity is O(2*n);
+// class MinStack {
+//     private Stack<int[]> stk;
+//     public MinStack() {
+//         stk = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(stk.isEmpty()){
+//             stk.push(new int[]{val,val});
+//         }
+//         else{
+//             int prev_min = stk.peek()[1];
+//             stk.push(new int[]{val,Math.min(val,prev_min)});
+//         }
+//     }
+    
+//     public void pop() {
+//         stk.pop();
+//     }
+    
+//     public int top() {
+
+//         return stk.peek()[0];
+//     }
+    
+//     public int getMin() {
+//         return stk.peek()[1];
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
