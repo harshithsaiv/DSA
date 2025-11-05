063716f5ac9a869b281ce7eba98740a2b53e8943
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-11-05 00:49:05 -0800

    0935-ORDERLY-QUEUE Stats: Time: 2 ms (99.69%), Space: 43.3 MB (90.57%) - LeetHub

--- 0935-orderly-queue/0935-orderly-queue.java
+++ 0935-orderly-queue/0935-orderly-queue.java
@@ -6,16 +6,39 @@ class Solution {
             return new String(a);
         }
         // k == 1: smallest rotation
-        return smallestRotationSimple(s);
+        return smallestRotationBooth(s);
         
     }
 
-    private String smallestRotationSimple(String s) {
-        String best = s;
-        for (int i = 1; i < s.length(); i++) {
-            String rot = s.substring(i) + s.substring(0, i);
-            if (rot.compareTo(best) < 0) best = rot;
+    // private String smallestRotationSimple(String s) {
+    //     String best = s;
+    //     for (int i = 1; i < s.length(); i++) {
+    //         String rot = s.substring(i) + s.substring(0, i);
+    //         if (rot.compareTo(best) < 0) best = rot;
+    //     }
+    //     return best;
+    // }
+
+    private String smallestRotationBooth(String s) {
+        String ss = s + s;
+        int n = s.length();
+        int i = 0, j = 1, k = 0;
+
+        while (i < n && j < n && k < n) {
+            char a = ss.charAt(i + k), b = ss.charAt(j + k);
+            if (a == b) {
+                k++;
+            } else if (a > b) {
+                i = i + k + 1;
+                if (i == j) i++;
+                k = 0;
+            } else {
+                j = j + k + 1;
+                if (i == j) j++;
+                k = 0;
+            }
         }
-        return best;
+        int start = Math.min(i, j);
+        return ss.substring(start, start + n);
     }
 }
