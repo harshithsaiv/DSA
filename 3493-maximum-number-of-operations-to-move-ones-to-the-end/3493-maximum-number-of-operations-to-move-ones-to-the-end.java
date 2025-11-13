1ae73a1167e84e8bb391f9ca25aebb3430e5670d
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-11-12 18:59:45 -0800

    3493-MAXIMUM-NUMBER-OF-OPERATIONS-TO-MOVE-ONES-TO-THE-END Stats: Time: 9 ms (18.52%), Space: 47.4 MB (20.74%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,36 +1,57 @@
+// class Solution {
+//     public int maxOperations(String s) {
+//         int prefix = 0;
+//         char []chars= s.toCharArray();
+//         int n = s.length();
+//         int [] arr = new int[n];
+//         int []prefixSum = new int[n];
+//         for(int i=0;i<n;i++){
+//             if(chars[i]=='1'){
+//                 arr[i]=1;
+//             }
+//             else{
+//                 arr[i]=0;
+//             }
+//         }
+
+//         prefixSum[0]=arr[0];
+
+//         for(int i=1;i<n;i++){
+//             prefixSum[i] = prefixSum[i-1]+arr[i];
+//         }
+
+//         int count =0;
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i=0;i<n;i++){
+//             if(arr[i]==0){
+//                 set.add(prefixSum[i]);
+//             }
+//         }
+
+//         for(int num:set){
+//             count+=num;
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int maxOperations(String s) {
-        int prefix = 0;
-        char []chars= s.toCharArray();
-        int n = s.length();
-        int [] arr = new int[n];
-        int []prefixSum = new int[n];
-        for(int i=0;i<n;i++){
-            if(chars[i]=='1'){
-                arr[i]=1;
-            }
-            else{
-                arr[i]=0;
+        int ones = 0;                    // current prefix count of '1's
+        boolean[] seen = new boolean[s.length() + 1];  // seen[k] = some zero had k ones before it
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            } else { // char == '0'
+                seen[ones] = true;
             }
         }
 
-        prefixSum[0]=arr[0];
-
-        for(int i=1;i<n;i++){
-            prefixSum[i] = prefixSum[i-1]+arr[i];
+        int ans = 0;
+        for (int k = 0; k <= ones; k++) {
+            if (seen[k]) ans += k;
         }
-
-        int count =0;
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                set.add(prefixSum[i]);
-            }
-        }
-
-        for(int num:set){
-            count+=num;
-        }
-        return count;
+        return ans;
     }
 }
