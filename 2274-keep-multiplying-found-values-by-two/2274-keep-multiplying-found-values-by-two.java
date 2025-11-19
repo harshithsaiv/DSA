7de18f5ff0218f16e7c0f0d8afe74195e3d2163a
Harshith Sai V <68597202+harshithsaiv@users.noreply.github.com>
2025-11-19 12:35:25 -0800

    2274-KEEP-MULTIPLYING-FOUND-VALUES-BY-TWO Stats: Time: 4 ms (28.31%), Space: 46.3 MB (10.97%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -1,30 +1,45 @@
-class Solution {
-    public int findFinalValue(int[] nums, int original) {
-        Arrays.sort(nums);
+// class Solution {
+//     public int findFinalValue(int[] nums, int original) {
+//         Arrays.sort(nums);
 
-         boolean search = true;
-        while(search){
-           search = binarySearch(nums,original);
-           if(search){
-                original = original *2;
-           }
+//          boolean search = true;
+//         while(search){
+//            search = binarySearch(nums,original);
+//            if(search){
+//                 original = original *2;
+//            }
+//         }
+//         return original;
+//     }
+
+//     public boolean binarySearch(int[] nums, int target) {
+//     int low = 0, high = nums.length - 1;
+//     while (low <= high) {
+//         int mid = low + (high - low) / 2; 
+//         if (nums[mid] == target) {
+//             return true;
+//         } else if (nums[mid] < target) {
+//             low = mid + 1;
+//         } else {
+//             high = mid - 1;
+//         }
+//     }
+//     return false;
+//     }
+
+// }
+
+class Solution{
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> hset = new HashSet<>();
+        for(int num :nums){
+            hset.add(num);
+        }
+        
+        while(hset.contains(original)){
+            original = original *2;
         }
         return original;
-    }
-
-    public boolean binarySearch(int[] nums, int target) {
-    int low = 0, high = nums.length - 1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2; 
-        if (nums[mid] == target) {
-            return true;
-        } else if (nums[mid] < target) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-    return false;
     }
 
 }
